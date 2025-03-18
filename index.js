f020d7da8ebf0c4dd326fefc59cdd078ce7ddc64
Sam <ssamsanya3@gmail.com>
2025-03-18 13:01:09 +0300

    Dom Manipulation

--- index.js
+++ index.js
@@ -10,22 +10,9 @@ document.addEventListener("DOMContentLoaded", function(){
         rating: 5,
         comment: "Delicious!",
       },
+     
       {
         id: 2,
-        name: "Miso Ramen",
-        restaurant: "Menya",
-        image: "miso.jpg",
-        rating: 4,
-        comment: "Very flavorful!",
-      },
-      {
-        id: 3,
-        name: "Tonkotsu Ramen",
-        restaurant: "Ramen-ya",
-        image: "tonkotsu.jpg",
-      },
-      {
-        id: 4,
         name: "Spicy Miso Ramen",
         restaurant: "Ippudo",
         image: "naruto.jpg",
@@ -33,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function(){
         comment: "Rich and spicy broth!",
       },
       {
-        id: 5,
+        id: 3,
         name: "Shio Ramen",
         restaurant: "Santouka",
         image: "gyukotsu.jpg",
@@ -41,34 +28,86 @@ document.addEventListener("DOMContentLoaded", function(){
         comment: "Light and perfectly balanced!",
       },
       {
-        id: 6,
-        name: "Tantanmen",
+        id: 4,
+        name: "Kojiro",
         restaurant: "Afuri",
-        image: "",
+        image: "kojiro.jpg",
         rating: 4,
         comment: "Nutty and flavorful!",
       },
       {
-        id: 7,
-        name: "Tsukemen",
+        id: 5,
+        name: "Nirvana",
         restaurant: "Rokurinsha",
-        image: "tsukemen.png",
+        image: "nirvana.jpg",
         rating: 5,
         comment: "Thick noodles, amazing dipping sauce!",
       },
+      {
+        id: 6,
+        name: "",
+        restaurant: "",
+        image: "",
+        rating: '',
+        comment:""
+      }
     ];
 
-    // Changing images
-  const images = document.querySelectorAll('.myImportedImages');
-  const placeholder = document.getElementById('ramen-detail');
-  images.forEach((image) => {  
-    image.addEventListener('click', function(){
-      placeholder.src = image.src;
-      placeholder.alt = image.alt;
+const newImagesInside = document.getElementById('ramen-menu');
+const imageToBeChanged = document.getElementById('ramen-detail');
+const myRating = document.getElementById('myRating');
+const myComment = document.getElementById('myComment');
+
+  function displayRamens(){
+    for(let element of ramens){
+      const myImages = document.createElement('img');
+      myImages.src = element.image;
+      myImages.alt = element.name;
+      myImages.classList.add('my-images-loaded');
+
+      newImagesInside.appendChild(myImages);
+      
+    }
+  }
+
+  newImagesInside.addEventListener('click', handleClick)
+  function handleClick(event){
+    if(event.target.classList.contains('my-images-loaded')){
+        imageToBeChanged.src = event.target.src;
+        imageToBeChanged.alt = event.target.alt;
+        
+        const matchingSource = ramens.find((element) => element.image === event.target.src.slice(22))
+       myRating.innerText = matchingSource.rating;
+       myComment.innerText = matchingSource.comment;
+      
+    }
+  }
+
+  const button = document.getElementById('myButton');
+  const myName = document.getElementById('name');
+  const myRestaurant = document.getElementById('restaurant');
+  const myUrl = document.getElementById('image');
+  const myRatings = document.getElementById('rating');
+  const myComments = document.getElementById('comment');
+
+
+  function addSubmitListener(){
+    button.addEventListener('click', function(e){
+      e.preventDefault();
+      myName.push(ramens)
+      myRestaurant.push(ramens)
+      myUrl.push(ramens)
+      myRatings.push(ramens)
+      myComments.push(ramens)
+      
     });
-   
-  });
+  }
+
+
+  (function main(){
+    displayRamens();
+    addSubmitListener();
+  })();
   
-  
-  });
-  
+})
+
